e21b48c9bb0bd26e5bce6366ff735e65d386b85e
moinsoft <mmoin.official@gmail.com>
2021-12-24 23:56:54 +0600

    scrollTop function And Smooth Jump Animate Actived

--- js/main.js
+++ js/main.js
@@ -5,11 +5,44 @@ $(window).on('scroll', function () {
   // Sticky Navbar Active
 
   if ($(this).scrollTop() > 25) {
+
     $('.header').addClass('sticky');
+
   } else {
+
     $('.header').removeClass('sticky');
+
   }
 
+
+});
+
+
+$(window).scroll(function () {
+  
+  // Bottom To Top Jump
+
+  if ($(this).scrollTop() > 300) {
+
+    $('#jump').addClass('scroll-top');
+
+  } else {
+
+    $('#jump').removeClass('scroll-top');
+
+  }
+
+  // Smooth Jump Animate
+
+  $('#jump').click(function () {
+
+    $('html, body').animate({
+
+      scrollTop: 0
+
+    }, 500)
+    
+  })
 
 
 
