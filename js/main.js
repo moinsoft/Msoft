a1127e34878a2f902dda21191431892364870a34
moinsoft <mmoin.official@gmail.com>
2021-12-24 17:04:22 +0600

    Sticky Navbar Actived

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 
 
+$(window).on('scroll', function () {
+
+  // Sticky Navbar Active
+
+  if ($(this).scrollTop() > 25) {
+    $('.header').addClass('sticky');
+  } else {
+    $('.header').removeClass('sticky');
+  }
+
+
+
+
+
+});
+
 
 $(document).ready(function () {
 
